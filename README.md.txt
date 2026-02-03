f1550191f8a6900d8de5de4f386e1820aaa4caa0
Ajin <aaajinmkl@gmail.com>
2026-02-03 12:30:14 +0530

    Modified

---
+++ README.md.txt
@@ -0,0 +1,12 @@
+
+This project is created to practice Python programming for GIS applications.
+The goal is to analyze raster-based geospatial data using simple Python scripts.
+
+Tools
+- Python
+- Rasterio
+- NumPy
+- Matplotlib
+
+
+Ajin
---
+++ day1_basics.py
@@ -0,0 +1,19 @@
+project_name = 'GIS Python Practice Project'
+author = 'Ajin'
+
+print('Project:', project_name)
+print('Author:', author)
+
+lst_values = [30.5,31.2,32.8,29.9,33.1]
+
+print('/nLST Values:', lst_values)
+print('Maximum LST:', max(lst_values))
+print('Minimum LST:', min(lst_values))
+print('Average LST:',sum(lst_values)/len(lst_values))
+
+
+for temp in lst_values:
+    if temp > 32:
+        print(temp, '-> High Temperature Zone')
+    else:
+        print(temp, '-> Low Temperature Zone')
